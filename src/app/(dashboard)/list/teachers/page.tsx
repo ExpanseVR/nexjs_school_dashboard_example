b7a2b2b3a927da224f8d44af1f0268874a9873cf
Michael Hayter <michaelhayter@expansevrdev.com>
2024-10-31 20:33:42 +0800

    Initial layout of the list page

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,14 @@
+const TeachersListPage = () => {
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            {/* TOP */}
+            <div className=""></div>
+            {/* LIST */}
+            <div className=""></div>
+            {/* PAGINATION */}
+            <div className=""></div>
+        </div>
+    );
+}
+
+export default TeachersListPage;
