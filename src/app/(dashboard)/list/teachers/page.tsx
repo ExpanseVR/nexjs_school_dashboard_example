d63f5e5a9ad9c442557b5099d659137b604fe596
Michael Hayter <michaelhayter@expansevrdev.com>
2024-11-12 21:46:31 +0800

    fetch teahers by classId

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -5,7 +5,7 @@ import TableSearch from "@/components/TableSearch";
 import { ITEM_PER_PAGE } from "@/lib/config";
 import { role, teachersData } from "@/lib/data";
 import prisma from "@/lib/prisma";
-import { Class, Subject, Teacher } from "@prisma/client";
+import { Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -98,9 +98,30 @@ const TeachersListPage = async ({
 
     const p = page ? parseInt(page) : 1;
 
+    // URL PARAMS CONDITION
+
+    const query: Prisma.TeacherWhereInput = {}
+
+    if(queryParams){
+        for(const [key, value] of Object.entries(queryParams)){
+            if(value === undefined) continue;
+
+            switch(key){
+                case "classId":
+                    query.lessons = {
+                        some:{
+                            classId: parseInt(value)
+                        }
+                    }  
+                    break;
+            }
+        }
+    }
+
     const [data, count] = await prisma.$transaction([
 
         prisma.teacher.findMany({
+        where:query,
         include: {
             subjects: true,
             classes: true,
@@ -108,7 +129,7 @@ const TeachersListPage = async ({
         take: ITEM_PER_PAGE,
         skip: ITEM_PER_PAGE * (p - 1),
     }),
-        prisma.teacher.count(),
+        prisma.teacher.count({where:query}),
     ]);
 
     return (
